3db9df7619b633cc28ef99efb913aaa6a38b7e4d
Suyogpatil090913 <suyogpatil4562@gmail.com>
2026-09-21 20:53:09 +0530

    Add Animal class with sound and test methods

---
+++ animal.py
@@ -0,0 +1,11 @@
+class Animal: 
+    
+    def sound(self):
+     print("Animal sound")
+
+    def test(self):
+       print("This is a test method")
+
+if __name__ == "__main__":
+    object =Animal()
+    object.test()
